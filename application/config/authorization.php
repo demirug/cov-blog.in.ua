26606c6ad72c244720f945cbc3b3033a9aa7ed68
demirug <pm.antondolia@gmail.com>
2021-03-26 00:07:49 +0200

    Fixed regexp

--- application/config/authorization.php
+++ application/config/authorization.php
@@ -2,8 +2,8 @@
     return [
         'min-password-length' => 5,
         'max-password-length' => 30,
-        'password-filter' => '[A-Za-z0-9]',
+        'password-filter' => '/[A-Za-z0-9]/',
         'min-login-length' => 5,
         'max-login-length' => 13,
-        'login-filter' => '[A-Za-z0-9]',
+        'login-filter' => "/[A-Za-z0-9]/",
     ];
