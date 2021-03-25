1b4e76676bffce5893c8d211bd7dad7be692e556
demirug <pm.antondolia@gmail.com>
2021-03-25 18:42:28 +0200

    Connected database to all models

--- application/core/Model.php
+++ application/core/Model.php
@@ -1,12 +1,24 @@
 <?php
 
-
 namespace application\core;
 
+use application\libs\DataBase;
 
 abstract class Model
 {
 
+    public $database;
+
+    public function __construct()
+    {
+
+        if($this->requireDataBase() === true) {
+            $this->database = new DataBase();
+        }
+
+    }
+
+    public abstract function onInitialize();
     public abstract function requireDataBase();
 
 }
