90cf5e5971807837ce20e37bc7633bbf629e8239
demirug <pm.antondolia@gmail.com>
2021-03-25 18:40:44 +0200

    Created DataBase system

---
+++ application/libs/DataBase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace application\libs;
+
+class DataBase
+{
+
+    public $dbConnection;
+
+    public function __construct()
+    {
+        $params = require 'application/config/database.php';
+        $this->dbConnection = new \PDO("mysql:host=" . $params['host'] . ";dbname=" . $params['base'], $params['username'], $params['password']);
+    }
+
+    //Sample of usage database->query("INSERT INTO BlogRecords(blogid, title, text) VALUES (:id, :title, :text)", ['id' => 15, 'title' => 'MyTitle', 'text' => 'Text']);
+    public function query($sql, $params = []) {
+
+        $statement = $this->dbConnection->prepare($sql);
+        if(!empty($params)) {
+            foreach ($params as $key => $value) {
+                $statement->bindValue(':'. $key, $value);
+            }
+        }
+
+        $statement->execute();
+        return $statement;
+    }
+
+
+
+}
