2529abf1db9e83acb1ca88d5bd02bb21f6433380
demirug <pm.antondolia@gmail.com>
2021-04-02 19:48:26 +0300

    View of error pages updated

--- application/views/codes/403.php
+++ application/views/codes/403.php
@@ -1,1 +1,21 @@
-403 - Доступ закрыт
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen and (color)" href="/public/styles/error.css">
+    <title>403</title>
+</head>
+<body>
+<div class="error-wrap">
+    <div class="error">
+        <p align="center"> <img src="/public/images/errors/403.png" width="70%"> </p>
+        <h2 align="center">Forbidden</h2><br>
+            <div align="center">
+
+                <a href="/"><button class="but" >Back Home</button></a>
+            </div>
+    </div>
+</div>
+</body>
+</html>
--- application/views/codes/404.php
+++ application/views/codes/404.php
@@ -1,1 +1,21 @@
-404 - Страница не найдена
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="screen and (color)" href="/public/styles/error.css">
+    <title>404</title>
+</head>
+<body>
+<div class="error-wrap">
+    <div class="error">
+        <p align="center"> <img src="/public/images/errors/404.png" width="70%"> </p>
+        <h2 align="center">Sorry but the page has not been found</h2><br>
+        <div align="center">
+
+            <a href="/"><button class="but" >Back Home</button></a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
