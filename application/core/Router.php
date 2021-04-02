ffb41f9406ed4385dfe299a530455b16bd70a0fc
demirug <pm.antondolia@gmail.com>
2021-04-02 20:52:31 +0300

    Router optimized

--- application/core/Router.php
+++ application/core/Router.php
@@ -17,15 +17,15 @@ class Router
         $address = strtolower(trim($_SERVER['REQUEST_URI'], '/'));
         $url = explode('/', $address);
 
-        $isShortRoute = false;
-
         $controllerPath = 'application\controllers\\';
         $method = 'index_Action';
+        $args = $url;
 
         if(array_key_exists($url[0], $this->shortRoutes)) {
             $controllerPath .= $this->shortRoutes[$url[0]]['controller'] . '_Controller';
             $method = $this->shortRoutes[$url[0]]['action'] . '_Action';
-            $isShortRoute = true;
+
+            array_shift($args);
         } else {
 
             $controllerPath .= ucfirst($url[0]) . '_Controller';
@@ -34,20 +34,17 @@ class Router
                 View::error(404);
             }
 
+            $args = array_shift($url);
+
             if(isset($url[1])) {
 
                 if (method_exists($controllerPath, $url[1] . '_Action')) {
                     $method = $url[1] . '_Action';
+                    array_shift($args);
                 } else {
                     View::redirect('/' . $url[0]);
                 }
             }
-        }
-
-        $args = null;
-
-        if(count($url) > ($isShortRoute ? 1 : 2)) {
-            $args = array_slice($url, ($isShortRoute ? 1 : 2));
         }
 
        $controller = new $controllerPath(['controller' => array_key_exists($url[0], $this->shortRoutes) ? $this->shortRoutes[$url[0]]['controller'] : ucfirst($url[0]), 'action' => substr($method, 0, -7)]);
