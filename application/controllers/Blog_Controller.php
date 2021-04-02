0910c17d39eb7aafba99b8f4e4054d476959c677
demirug <pm.antondolia@gmail.com>
2021-04-02 20:58:46 +0300

    Created structure for blog controller

--- application/controllers/Blog_Controller.php
+++ application/controllers/Blog_Controller.php
@@ -3,16 +3,44 @@
 namespace application\controllers;
 
 use application\core\Controller;
+use application\core\View;
 
 class Blog_Controller extends Controller
 {
 
-    public function onInitialize()
-    {
-        // TODO: Implement onInitialize() method.
-    }
+    /* All pages
+     * cov-blog.in.ua/blogs/{userName}
+     * cov-blog.in.ua/blogs/{userName}/{blogName/blogid}/{page}
+     * cov-blog.in.ua/blogs/{regionName}
+     * cov-blog.in.ua/blogs/{regionName}/{page}
+     *
+     * cov-blog.in.ua/view/{userName}/{blogName/blogid}
+     *
+     * cov-blog.in.ua/edit/{blogid}
+     * cov-blog.in.ua/add
+    */
+
+    public function onInitialize() {}
 
     public function index_Action($args) {
-        print_r($args);
+        View::redirect('/blogs');
     }
+
+    public function blogs_Action($args) {
+
+    }
+
+    public function view_Action($args) {
+
+    }
+
+    public function edit_Action($args) {
+
+    }
+
+    public function add_Action($args) {
+
+    }
+
+
 }
