e16304cae840f7d6cc6bd9b3895d5d9aa0cfb875
demirug <pm.antondolia@gmail.com>
2021-04-04 21:17:48 +0300

    Created logic for view blogs list/blog records

--- application/controllers/Blog_Controller.php
+++ application/controllers/Blog_Controller.php
@@ -8,13 +8,14 @@ use application\core\View;
 class Blog_Controller extends Controller
 {
 
-    /* All pages
+    /*
      * cov-blog.in.ua/blogs/{userName}
      * cov-blog.in.ua/blogs/{userName}/{blogName/blogid}/{page}
+     *
      * cov-blog.in.ua/blogs/{regionName}
      * cov-blog.in.ua/blogs/{regionName}/{page}
      *
-     * cov-blog.in.ua/view/{userName}/{blogName/blogid}
+     * cov-blog.in.ua/view/{userName}/{blogName/blogid}/{page}
      *
      * cov-blog.in.ua/edit/{blogid}
      * cov-blog.in.ua/add
@@ -23,15 +24,88 @@ class Blog_Controller extends Controller
     public function onInitialize() {}
 
     public function index_Action($args) {
-        View::redirect('/blogs');
+        View::redirect('/');
     }
 
     public function blogs_Action($args) {
 
+        $argsLength = count($args);
+
+        if($argsLength == 0) {
+            View::redirect('/');
+        } else {
+
+            $blogConfig = require 'application/config/blog.php';
+            $blogsPerPage = $blogConfig['blog-list']['blogs-rows'] * $blogConfig['blog-list']['blogs-columns'];
+            $pageNumber = 1;
+
+            if($argsLength > 1 && is_numeric($args[1]) && $args[1] >= 1) {
+                $pageNumber = $args[1];
+            }
+
+
+            if($this->model->hasRegion($args[0])) {
+
+                $result = $this->model->getBlogsByRegion($args[0], $pageNumber, $blogsPerPage);
+                echo "Блоги пользователей по области: $args[0] | Страница: $pageNumber <hr>";
+
+                foreach ($result as $value) {
+                    echo 'Author: ' . $value['username'] . ' | ' .$value['title'] . ' | ' . $value['description'] . ' | Created: ' . $value['createDate'] . '<br>';
+                    echo "<span>To read blog <a href='/view/" . $value['username'] . '/' . str_replace(' ', '-', $value['title']) . "'>click here</a></span><br><hr>";
+                }
+
+
+            } else {
+
+                $result = $this->model->getBlogsByUser($args[0], $pageNumber, $blogsPerPage);
+                if(isset($result)) {
+
+                    echo "Блоги пользователя: $args[0] | Страница: $pageNumber <hr>";
+
+                    foreach ($result as $value) {
+                        echo  $value['title'] . ' | ' . $value['description'] . ' | Created: ' . $value['createDate'] . '<br>';
+                        echo "<span>To read blog <a href='/view/$args[0]/" . str_replace(' ', '-', $value['title']) . "'>click here</a></span><br><hr>";
+                    }
+
+                } else {
+                    View::error(404);
+                }
+            }
+        }
     }
 
     public function view_Action($args) {
+        //cov-blog.in.ua/view/demirug/blogName
+        $argsLength = count($args);
 
+        if($argsLength == 0) {
+            View::redirect('/');
+        }
+
+        if($argsLength == 1) {
+            View::redirect('/blogs/' . $args[0]);
+        }
+
+        $userID = $this->model->getUserID($args[0]);
+
+        if($userID === -1) {
+            View::error(404);
+        }
+
+        $blogID = $this->model->getBlogIDByName($userID, str_replace('-', ' ', $args[1]));
+        if($blogID === -1) {
+            View::redirect('/blogs/' . $args[0]);
+        }
+
+        $result = $this->model->getPostsByUser($blogID, 1, 5);
+
+        echo "<center><h1>" . str_replace('-', ' ', $args[1]) ."</h1></center>";
+
+        foreach ($result as $value) {
+
+            echo "<hr><h3>".$value['title']. "</h3>" . "<span>" . $value['text'] . "</span><br>" . $value['createDate'] . "<hr>";
+
+        }
     }
 
     public function edit_Action($args) {
--- application/models/Blog_Model.php
+++ application/models/Blog_Model.php
@@ -7,13 +7,66 @@ use application\core\Model;
 class Blog_Model extends Model
 {
 
-    public function onInitialize()
-    {
-        // TODO: Implement onInitialize() method.
+    public static function regionList() {
+        return [
+            'vinnytsia',
+            'dnipro',
+            'donetsk',
+            'kiev',
+            'lutsk',
+            'lviv',
+            'odesa',
+            'simferopol',
+            'kharkiv'
+        ];
     }
+
+
+    public function hasRegion($regName) {
+        return in_array($regName, Blog_Model::regionList());
+    }
+
+    public function getUserID($username) {
+
+        $statement = $this->database->query('SELECT id, login FROM Users WHERE login = :userName', ['userName' => $username]);
+        $result = $statement->fetch(\PDO::FETCH_ASSOC);
+        return ($result ? $result['id'] : -1);
+    }
+
+    public function getBlogsByUser($userName, $pageNumber, $blogsPerPage) {
+
+        $userID = $this->getUserID($userName);
+
+        if($userID === -1) { return null; }
+
+        $statement = $this->database->query("SELECT * FROM BlogList WHERE userId = '$userID' LIMIT $blogsPerPage OFFSET " . ($pageNumber - 1) * $blogsPerPage);
+
+        return  $statement->fetchAll();
+    }
+
+
+    public function getBlogIDByName($userID, $blogName) {
+        $statement = $this->database->query("SELECT login as username, blogid FROM BlogList JOIN Users ON userID = '$userID' AND title = '$blogName'");
+        $result = $statement->fetch(\PDO::FETCH_ASSOC);
+        return ($result ? $result['blogid'] : -1);
+    }
+
+    public function getPostsByUser($blogID, $pageNumber, $blogsPerPage) {
+        $statement = $this->database->query("SELECT * FROM BlogRecords WHERE blogid = $blogID ORDER BY createDate");
+        return  $statement->fetchAll();
+    }
+
+    public function getBlogsByRegion($regName, $pageNumber, $blogsPerPage) {
+
+        $statement = $this->database->query("SELECT login as username, title, description, createDate, region FROM BlogList JOIN Users ON userID = id WHERE region = '$regName' LIMIT  $blogsPerPage OFFSET " . ($pageNumber - 1) * $blogsPerPage);
+        return $statement->fetchAll();
+
+    }
+
+    public function onInitialize() {}
 
     public function requireDataBase()
     {
-        // TODO: Implement requireDataBase() method.
+       return true;
     }
 }
