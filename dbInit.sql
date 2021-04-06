42f6f4c2831499f6fc4d68902b636fdbe4dfd998
demirug <pm.antondolia@gmail.com>
2021-04-07 00:45:05 +0300

    Change DATE to TIMESTAMP in database

--- dbInit.sql
+++ dbInit.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS Users (
                                      `sault` varchar(16) NOT NULL,
                                      `email` varchar(320) NOT NULL,
                                      `permissionLevel` TINYINT DEFAULT 0,
-                                     `registerDate` DATE DEFAULT (CURRENT_DATE),
+                                     `registerDate` TIMESTAMP DEFAULT (CURRENT_TIMESTAMP),
                                      PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE utf8_bin AUTO_INCREMENT=0
 
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS BlogList (
                                         `title` varchar(180) NOT NULL,
                                         `description` varchar(1200) NOT NULL,
                                         `region` varchar(16) NOT NULL,
-                                        `createDate` DATE DEFAULT (CURRENT_DATE),
+                                        `createDate` TIMESTAMP DEFAULT (CURRENT_TIMESTAMP),
                                         PRIMARY KEY (`blogid`)
 ) DEFAULT CHARSET=utf8 COLLATE utf8_bin AUTO_INCREMENT=0
 
@@ -25,5 +25,5 @@ CREATE TABLE IF NOT EXISTS BlogRecords (
                                            `blogid` int NOT NULL,
                                            `title` varchar(180) NOT NULL,
                                            `text` varchar(5000) NOT NULL,
-                                           `createDate` DATE DEFAULT (CURRENT_DATE)
+                                           `createDate` TIMESTAMP DEFAULT (CURRENT_TIMESTAMP)
 ) DEFAULT CHARSET=utf8 COLLATE utf8_bin
