672be741b8c18369f1f53958dbb40a7a9196bacd
demirug <pm.antondolia@gmail.com>
2021-04-24 21:50:58 +0300

    Updated login/register page style

--- application/views/Account/login.php
+++ application/views/Account/login.php
@@ -1,33 +1,30 @@
-<center>
-    <div style="background-color: aqua; width: 600px; height: 350px; border-radius: 10px;">
-        <br>
-        <h3>Login in your account</h3>
-
-        <form action="/login" method="post">
-
-            <div>
-                <input type="text" style="padding: 12px 20px; margin: 8px 0" name="login" placeholder="Enter login" regex="[A-Za-z0-9]" required>
-            </div>
-            <div>
-                <input type="password" style="padding: 12px 20px; margin: 8px 0" name="password" placeholder="Enter password" required>
-            </div>
-            <div>
-                <input type="submit" style="padding: 12px 20px; margin: 8px 0" name="submit", value="Submit">
-            </div>
-
-        </form>
-
-        <div>
-            <span>Forgot</span>
-            <a href="/restore">Password?</a>
+<div class="login-wrap">
+    <div class="login-html">
+        <div class="login-form">
+            <h2 align="center">Вход</h2>
+            <form class="sign-in-htm" action="/login" method="post">
+                <div class="group">
+                    <label for="login" class="label">Логин</label>
+                    <input id="login" name="login" type="text" class="input" regex="[A-Za-z0-9]" required>
+                </div>
+                <div class="group">
+                    <label for="pass" class="label">Пароль</label>
+                    <input id="pass" name="password" type="password" class="input" data-type="password" required>
+                </div>
+                <div class="group">
+                    <input type="hidden" name="isPermanent" value="0">
+                    <input id="check" name="isPermanent" value="1" type="checkbox" class="check" checked>
+                    <label for="check"><span class="icon"></span> Запомнить меня</label>
+                </div>
+                <div class="group">
+                    <input type="submit" class="button" value="Войти">
+                </div>
+                <output class="error-message"></output>
+                <div class="hr"></div>
+                <div class="foot-lnk">
+                    <a href="/register">Создать аккаунт</a>
+                </div>
+            </form>
         </div>
-
-        <div style="margin-top: 20px; margin-right: 20px; text-align: right">
-            <a href="/register">Create account</a>
-        </div>
-
-
     </div>
-
-
-</center>
+</div>
--- application/views/Account/register.php
+++ application/views/Account/register.php
@@ -1,29 +1,33 @@
-<center>
-    <div style="background-color: aqua; width: 600px; height: 350px; border-radius: 10px;">
-        <br>
-        <h3>Register</h3>
-
-        <form action="/register" method="post">
-
-            <div>
-                <input type="text" style="padding: 12px 20px; margin: 8px 0" name="login" placeholder="Enter login" regex="[A-Za-z0-9]" required>
-            </div>
-            <div>
-                <input type="password" style="padding: 12px 20px; margin: 8px 0" name="password" placeholder="Enter password" required>
-            </div>
-            <div>
-                <input type="password" style="padding: 12px 20px; margin: 8px 0" name="conf_password" placeholder="Confirm password" required>
-            </div>
-            <div>
-                <input type="submit" style="padding: 12px 20px; margin: 8px 0" name="submit", value="Submit">
-            </div>
-
-        </form>
-
-        <div>
-            <span>Already registered? </span>
-            <a href="/login">Login</a>
+<div class="sign-wrap">
+    <div class="login-html">
+        <div class="login-form">
+            <h2 align="center">Регистрация</h2>
+            <form class="sign-in-htm" action="/register" method="post">
+                <div class="group">
+                    <label for="login" class="label">Логин</label>
+                    <input id="login" name="login" type="text" class="input" regex="[A-Za-z0-9]" required>
+                </div>
+                <div class="group">
+                    <label for="email" class="label">Электронный адрес</label>
+                    <input id="email" name="email" type="email" class="input" required>
+                </div>
+                <div class="group">
+                    <label for="pass" class="label">Пароль</label>
+                    <input id="pass" name="password" type="password" class="input" data-type="password" required>
+                </div>
+                <div class="group">
+                    <label for="conf_pass" class="label">Пароль еще раз</label>
+                    <input id="conf_pass" name="conf_password" type="password" class="input" data-type="password" required>
+                </div>
+                <div class="group">
+                    <input type="submit" class="button" value="Зарегистрироваться">
+                </div>
+                <output class="error-message"></output>
+                <div class="hr"></div>
+                <div class="foot-lnk">
+                    <a href="/login">Уже зарегистрированы?</a>
+                </div>
+            </form>
         </div>
-
     </div>
-</center>
+</div>
