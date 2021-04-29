adfa8c3bbca890d4c920f2a28274b08fa6e36bf1
demirug <pm.antondolia@gmail.com>
2021-04-30 02:15:28 +0300

    Changed config of blog

--- application/config/blog.php
+++ application/config/blog.php
@@ -1,17 +1,10 @@
 <?php
-    //Pagination settings for blog
     return [
 
-        //Count of rows and columns of blogs that can be displayed per page
-        'blog-list' => [
-            'blogs-rows' => 1,
-            'blogs-columns' => 3,
-        ],
-
+        //Count of blogs that can be displayed per page
+        'blog-list' => 5,
         //Count of blog records per page
-        'blog-user' => [
-            'blog-records' => 10,
-        ],
+        'blog-view' => 10,
 
 
     ];
