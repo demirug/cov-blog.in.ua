92a6cd8664aafe3f90a85512ad2a8db8c7a89664
demirug <pm.antondolia@gmail.com>
2021-05-05 17:17:04 +0300

    Pagination view/style updated

--- application/views/pagination/default.php
+++ application/views/pagination/default.php
@@ -1,17 +1,32 @@
-<center>
-    <?php if($pageCount != 0): ?>
+<?php if($pageCount > 1): ?>
 
-        <?php if($curPage != 1): ?>
-            <a href="<?php echo $url . '/' . ($curPage - 1); ?>" class = "control" >Previous</a>
-        <?php endif; ?>
+    <div class="pagination">
+    <center>
 
-        <?php  for ($i = 1; $i <= $pageCount; $i++): ?>
-            <a href="<?php echo $url . '/' . $i; ?>" <?php if($i == $curPage) { echo "style='selected'"; } ?> ><?php echo $i; ?></a>
-        <?php  endfor; ?>
+            <?php
 
-        <?php if($curPage != $pageCount): ?>
-            <a href="<?php echo $url . '/' . ($curPage + 1); ?>" class = "control" >Next</a>
-        <?php endif; ?>
+            if($curPage <= 1) {
+                echo "<a role='button' class = 'control lock' >Previous</a>";
+            } else {
+                echo "<a href='$url/" . ($curPage - 1) . "' class = 'control'>Previous</a>";
+            }
 
-    <?php endif; ?>
-</center>
+            ?>
+
+            <?php  for ($i = 1; $i <= $pageCount; $i++): ?>
+                <a href="<?php echo $url . '/' . $i; ?>" <?php if($i == $curPage) { echo "class='selected'"; } ?> ><?php echo $i; ?></a>
+            <?php  endfor; ?>
+
+            <?php
+
+            if($curPage >= $pageCount) {
+                echo "<a role='button' class = 'control lock' >Next</a>";
+            } else {
+                echo "<a href='$url/" . ($curPage + 1) . "' class = 'control'>Next</a>";
+            }
+
+            ?>
+    </center>
+    </div>
+
+<?php endif; ?>
