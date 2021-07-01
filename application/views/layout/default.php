2e8487ac09afd235483c30440fccc9d632b3ad3b
demirug <pm.antondolia@gmail.com>
2021-07-01 11:11:14 +0300

    Created navigation panel

--- application/views/layout/default.php
+++ application/views/layout/default.php
@@ -6,7 +6,7 @@
     <title> <?php echo $title; ?> </title>
 
     <link rel="stylesheet" href="/public/styles/modal.css">
-
+    <link rel="stylesheet" href="/public/styles/navigation.css">
     <?php foreach($css as $path): ?>
         <link rel="stylesheet" href=<?php echo $path; ?>>
     <?php endforeach; ?>
@@ -42,6 +42,47 @@
 </div>
 <!-- Modal end -->
 
+<!-- Navigation -->
+<div id="header">
+    <div class="logo">
+        <a href="/" ><img class="d-block mx-auto" src="https://res.cloudinary.com/doytulo2j/image/upload/v1621761692/%D0%BA%D0%BE%D1%80%D0%BE%D0%BD%D0%B0/cov_blog_da1wvc.png" width="230" draggable="false" alt="Logo"></a>
+    </div>
+    <nav>
+        <ul>
+            <li class="dropdown">
+                <a> Blogs</a>
+                <ul>
+                    <li><a href="/blogs/vinnytsia">Vinnytsia</a></li>
+                    <li><a href="/blogs/dnipro">Dnipro</a></li>
+                    <li><a href="/blogs/donetsk">Donetsk</a></li>
+                    <li><a href="/blogs/kharkiv">Kharkiv</a></li>
+                    <li><a href="/blogs/kiev">Kiev</a></li>
+                    <li><a href="/blogs/lutsk">Lutsk</a></li>
+                    <li><a href="/blogs/lviv">Lviv</a></li>
+                    <li><a href="/blogs/odesa">Odessa</a></li>
+                    <li><a href="/blogs/simferopol">Simferopol</a></li>
+                </ul>
+            </li>
+            <?php if(isset($_SESSION['userID'])): ?>
+                <li>
+                    <a  href="/account/settings"></i>Account</a>
+                </li>
+                <li>
+                    <a  href="/logout"></i>Logout</a>
+                </li>
+            <?php else: ?>
+                <li>
+                    <a  href="/login"></i>Sign in</a>
+                </li>
+                <li>
+                    <a  href="/register"></i>Sign up</a>
+                </li>
+            <?php endif; ?>
+        </ul>
+    </nav>
+</div>
+<br/>
+<!-- Navigation end -->
 
 <?php echo $content; ?>
 
